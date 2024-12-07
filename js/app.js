42366d4b1fb09a8940de1f22417baba207f16a80
XXX <vitya_maksimov_22@mail.ru>
2024-12-07 17:33:01 +0700

    Added functionality for slider, smooth scrolling, burger button and modal windows

--- js/app.js
+++ js/app.js
@@ -46,3 +46,45 @@ if (burgerButton && navigationMenu) {
 		});
 	});
 }
+
+// modal
+const modalController = ({modal, btnOpen, btnClose}) => {
+	const buttonEl = document.querySelectorAll(btnOpen);
+	const modalEl = document.querySelector(modal);
+
+	modalEl.style.cssText = `
+		display: flex;
+		visibility: hidden;
+		line-height: 1.3;
+		opacity: 0;
+		transition: opacity 300ms ease-in-out;
+		z-index: 9999;
+	`;
+
+	const openModal = () => {
+		modalEl.style.visibility = 'visible';
+		modalEl.style.opacity = 1;
+	};
+
+	const closeModal = event => {
+		const target = event.target;
+
+		if (target === modalEl || target.closest(btnClose)) {
+			modalEl.style.opacity = 0;
+			setTimeout(() => { modalEl.style.visibility = 'hidden'; }, 300);
+		}
+	};
+
+	buttonEl.forEach(btn => { btn.addEventListener('click', openModal); });
+	modalEl.addEventListener('click', closeModal);
+};
+
+const modals = [
+	{modal: '.modal1', btnOpen: '.section__btn1'},
+	{modal: '.modal2', btnOpen: '.section__btn2'},
+	{modal: '.modal3', btnOpen: '.section__btn3'},
+];
+
+modals.forEach(({modal, btnOpen}) => {
+	modalController({modal, btnOpen, btnClose: '.modal__close'});
+});
