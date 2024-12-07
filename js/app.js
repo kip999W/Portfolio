d4d2949dabc20c0799f3f2e4990d097a712837f7
XXX <vitya_maksimov_22@mail.ru>
2024-12-07 14:06:46 +0700

    Added Swiper functionality, scrolling and burger button control

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// swiper
 const swiperText = new Swiper('.swiper', {
 	speed: 1600,
 	allowTouchMove: false,
@@ -12,3 +13,36 @@ const swiperText = new Swiper('.swiper', {
 		nextEl: '.swiper-button-next'
 	}
 });
+
+// smooth scroll
+const anchors = document.querySelectorAll('a[href*="#"]');
+anchors.forEach(anchor => {
+	anchor.addEventListener('click', event => {
+   	event.preventDefault();
+
+   	const blockID = anchor.getAttribute('href').substring(1);
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
+
+// burger
+const burgerButton = document.querySelector('.burger');
+const navigationMenu = document.querySelector('.header-nav');
+const navLinks = navigationMenu.querySelectorAll('a');
+
+if (burgerButton && navigationMenu) {
+	burgerButton.addEventListener('click', () => {
+		burgerButton.classList.toggle('active');
+		navigationMenu.classList.toggle('open');
+	});
+
+	navLinks.forEach(link => {
+		link.addEventListener('click', () => {
+			burgerButton.classList.remove('active');
+			navigationMenu.classList.remove('open');
+		});
+	});
+}
